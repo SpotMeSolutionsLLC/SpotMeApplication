aec22c84097891316c068b60b7fc3a81f8f4dd32
James Zhao <zhaojyl935@gmail.com>
2018-11-01 23:28:47 -0700

    Fixed Map for android:

--- src/components/MapScreen.js
+++ src/components/MapScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, TouchableHighlight, Image } from 'react-native';
+import { Dimensions, StyleSheet, View, Text, TouchableHighlight, Image } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { connect } from 'react-redux';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
@@ -23,19 +23,25 @@ import carMarker from '../images/car.png';
 class MapScreen extends Component {
     constructor(props) {
       super(props);
-      
       this.state = {
-        lat: this.currentLocation.lat,
-        long: this.currentLocation.long
+        isMapReady: false,
+        lat: 37.339222,
+        long: -121.880724,
       };
     }
 
+
     componentWillMount() {
       this.props.getCurrentLocation();
+
     }
 
     componentDidMount() {
       console.log(this.state);
+    }
+
+    onMapLayout = () => {
+      this.setState({isMapReady: true});
     }
 
     render() {
@@ -55,102 +61,111 @@ class MapScreen extends Component {
   
             <Image source={require('../images/icon.jpg')} />
           </View>
-  
-          <View style={styles.container}>
-            {this.props.currentLocation.latitude && (
-              <MapView
-                provider={PROVIDER_GOOGLE}
-                style={styles.map}
-                region={{
-                  latitude: currentInstance.state.lat,
-                  longitude: currentInstance.state.long,
-                  latitudeDelta: 0.0112,
-                  longitudeDelta: 0.01412
-                }}
-                customMapStyle={MidnightCommander}
-                //customMapStyle={DarkMapStyles}
-              > 
+          {console.log("test")}
+          <View style={styles.contianer}>
+            <GooglePlacesAutocomplete
+              placeholder='Search a location or garage!' 
+              minLength={2} //Minimum length of text entered for autocomplete results
+              autoFocus={false}
+              listViewDisplayed='false'
+              returnKeyType={'default'}
+              fetchDetails
+              renderDescription={row => row.description}
+              onPress={(data, details = null) => {
+                //console.log(details.address_components);
+                //console.log(details.geometry.location);
+                //console.log(details.geometry.location.lat);
+                //console.log(details.geometry.location.lng);
+                //console.log(data.description);
+                //console.log('Reached');
+            
+                currentInstance.state = {
+                  //Latitude and Longitude
+                  lat: details.geometry.location.lat,
+                  long: details.geometry.location.lng,
+                  //Title
+                  description: data.description
+                };
 
+
+                // currentInstance.state.current.forceUpdate();
+
+                // console.log("Lat" + currentInstance.state.lat);
+
+                return details;
+              }}
+              getDefaultValue={() => ''}
+              query={{ key: 'AIzaSyAknyin7pzbkZ89IRg6QeQ0gC2sVjSKRpY' }}
+              styles={{
+                textInputContainer: {
+                  width: '100%',
+                  backgroundColor: '#42b8ba',
+                  //backgroundColor: 'transparent'
+                  zIndex: 99
+                },
+                listView: {
+                  position:"absolute",
+                  backgroundColor: 'white',
+                  //backgroundColor: 'transparent',
+                  // height: Dimensions.get("window").height,
+                  zIndex:99,
+                  top: 40
+                },
+                description: {
+                  fontWeight: 'bold',
+                  fontSize: 18,
+                  height: 40
+                  //color: 'white'
+                },
+              }}
+            />
+
+            <MapView
+              provider={PROVIDER_GOOGLE}
+              style={styles.map}
+              region={{
+                latitude: currentInstance.state.lat,
+                longitude: currentInstance.state.long,
+                latitudeDelta: 1,
+                longitudeDelta: 1
+              }}
+              //customMapStyle={MidnightCommander}
+              onLayout = {this.onMapLayout}
+              //customMapStyle={DarkMapStyles}
+            >
+              { this.state.isMapReady && 
+                <View>
                   <MapView.Marker 
-                  coordinate={{ latitude: currentInstance.state.lat, longitude: currentInstance.state.long }}
-                  description={currentInstance.state.description}
-                  //image={carMarker}
-                  //description={this.props.sanjose.garageAvailable}
-                  />
-
-                  <MapView.Marker
-                        coordinate={{ latitude: 37.339222, longitude: -121.880724, }}
-                        //Can later pull coord, title, descrip from API when implemented
-                        title={'SJSU North Parking Garage'}
-                        description={'Spots Filled: 977/1490'}
-                        image={garageMarker}
+                    coordinate={{ latitude: this.state.lat, longitude: this.state.long }}
+                    description={this.state.description}
                   />
                   <MapView.Marker
-                      coordinate={{ latitude: 37.332303, longitude: -121.882986, }}
-                      title={'SJSU West Parking Garage'}
-                      description={'Spots Filled: 827/1135'}
-                      image={garageMarker}
+                    coordinate={{ latitude: 37.339222, longitude: -121.880724, }}
+                    //Can later pull coord, title, descrip from API when implemented
+                    title={'SJSU North Parking Garage'}
+                    description={'Spots Filled: 977/1490'}
+                    image={garageMarker}
                   />
                   <MapView.Marker
-                      coordinate={{ latitude: 37.333088, longitude: -121.880797, }}
-                      title={'SJSU South Parking Garage'}
-                      description={'Spots Filled: 1377/1500'}
-                      image={garageMarker}
+                    coordinate={{ latitude: 37.332303, longitude: -121.882986, }}
+                    title={'SJSU West Parking Garage'}
+                    description={'Spots Filled: 827/1135'}
+                    image={garageMarker}
                   />
-
-              <GooglePlacesAutocomplete
-                placeholder='Search a location or garage!' 
-                minLength={2} //Minimum length of text entered for autocomplete results
-                autoFocus={false}
-                listViewDisplayed='false'
-                returnKeyType={'default'}
-                fetchDetails
-                renderDescription={row => row.description}
-                onPress={(data, details = null) => {
-                  console.log(data, details);
-                  //console.log(details.address_components);
-                  //console.log(details.geometry.location);
-                  //console.log(details.geometry.location.lat);
-                  //console.log(details.geometry.location.lng);
-                  //console.log(data.description);
-                  //console.log('Reached');
-              
-                  currentInstance.setState({
-                    //Latitude and Longitude
-                    lat: details.geometry.location.lat,
-                    long: details.geometry.location.lng,
-                    //Title
-                    description: data.description
-                  });
-
-                  // console.log("Lat" + currentInstance.state.lat);
-
-                  return details;
-                }}
-                getDefaultValue={() => ''}
-                query={{ key: 'AIzaSyAknyin7pzbkZ89IRg6QeQ0gC2sVjSKRpY' }}
-                styles={{
-                  textInputContainer: {
-                    width: '100%',
-                    backgroundColor: '#42b8ba'
-                    //backgroundColor: 'transparent'
-                  },
-                  listView: {
-                    backgroundColor: 'white',
-                    //backgroundColor: 'transparent',
-                    height: '100%'
-                  },
-                  description: {
-                    fontWeight: 'bold',
-                    fontSize: 18,
-                    //color: 'white'
-                  },
-                }}
-              />
-
-              </MapView>
-            )}
+                  <MapView.Marker
+                    coordinate={{ latitude: 37.333088, longitude: -121.880797, }}
+                    title={'SJSU South Parking Garage'}
+                    description={'Spots Filled: 1377/1500'}
+                    image={garageMarker}
+                  />
+                  
+                </View>
+              }
+            </MapView>
+            
           </View>
+
+
         </View>
       );
     }
@@ -160,15 +175,21 @@ class MapScreen extends Component {
     container: {
       flex: 1,
       justifyContent: 'center',
-      alignItems: 'center'
+      alignItems: 'center',
     },
     map: {
-      ...StyleSheet.absoluteFillObject
+      ...StyleSheet.absoluteFillObject,
+      flex: 1,
+      width: Dimensions.get("window").width,
+      height: Dimensions.get("window").height
     },
     outerContainer: {
       flex: 1,
       flexDirection: 'column',
-      justifyContent: 'center'
+      justifyContent: 'flex-start',
+      borderRadius: 2,
+      borderWidth: 2,
+      borderColor: "#d6d7da"
     },
     companyText: {
       fontSize: 30,
